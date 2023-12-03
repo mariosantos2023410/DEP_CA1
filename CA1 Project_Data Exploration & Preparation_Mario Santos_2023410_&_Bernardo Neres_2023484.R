4c87d84b63e20f3e954bf9ba27b52cd3e98b73e6
Mario Santos <145356072+mariosantos2023410@users.noreply.github.com>
2023-12-03 04:39:30 +0000

    Add files via upload

--- CA1 Project_Data Exploration & Preparation_Mario Santos_2023410_&_Bernardo Neres_2023484.R
+++ CA1 Project_Data Exploration & Preparation_Mario Santos_2023410_&_Bernardo Neres_2023484.R
@@ -12,7 +12,6 @@ view(covid_dataset)
 #This shows how many rows and columns this data set has.
 dim(covid_dataset)
 
-
 #This displays the name of the variables
 names(covid_dataset)
 
@@ -35,18 +34,38 @@ print(mv_percentage)
 # This is to remove the "Note" column from the original data set and will generate a new data set without the column "Note"
 #This column is being removed because the entire column is fulfilled of missing values
 covid_New <- covid_dataset[, !(names(covid_dataset) %in% c("note"))]
+view(covid_New)
 
-# This is to replace missing values with the mode for the "country_code" variable
+# This is to identify duplicates on specific columns
+duplicates <- duplicated(covid_New[, c("country", "country_code", "continent",
+                                       "population", "indicator", "weekly_count", "year_week", "rate_14_day", 
+                                       "cumulative_count", "source")])
+
+#This is to Localize duplicates in the data frame
+duplicates_rows <- covid_New[duplicates, ]
+
+#This is to print the data frame with duplicates
+print(duplicates_rows)
+
+#This is to replace missing values with the mode for the variables
 covid_New$country_code <- ifelse(is.na(covid_New$country_code), mode(covid_New$country_code), covid_New$country_code)
+covid_New$weekly_count <- ifelse(is.na(covid_New$weekly_count), mean(covid_New$weekly_count, na.rm = TRUE), covid_New$weekly_count)
+covid_New$rate_14_day <- ifelse(is.na(covid_New$rate_14_day), mean(covid_New$rate_14_day, na.rm = TRUE), covid_New$rate_14_day)
+covid_New$cumulative_count <- ifelse(is.na(covid_New$cumulative_count), mean(covid_New$cumulative_count, na.rm = TRUE), covid_New$cumulative_count)
+
+
+#This is to check the missing values after handling and treating them.
+mv <- colSums(is.na(covid_New))
+mv
+view(covid_New)
 
 # This is to identify variable types
 categorical_cols <- c("country", "country_code", "continent", "indicator", "year_week", "source")  # Categorical variables
 discrete_cols <- c("weekly_count", "cumulative_count")  # Discrete variables
 continuous_cols <- c("population", "rate_14_day")  # Continuous variables
 
-
-# This is to create a data frame to represent variables types if they are 
-# Categorical, Discrete or Continuous
+#This is to create a data frame to represent variables types if they are 
+#Categorical, Discrete or Continuous
 variable_types <- data.frame(
   Variable = c(categorical_cols, discrete_cols, continuous_cols),
   Type = c(rep("Categorical", length(categorical_cols)),
@@ -54,7 +73,7 @@ variable_types <- data.frame(
            rep("Continuous", length(continuous_cols)))
 )
 
-# This is to create a bar plot to visualize variable types
+#This is to create a bar plot to visualize variable types
 ggplot(variable_types, aes(x = Variable, fill = Type)) +
   geom_bar(position = "dodge", stat = "count") +
   labs(title = "Variable Types Bar Plot",
@@ -66,38 +85,115 @@ ggplot(variable_types, aes(x = Variable, fill = Type)) +
 #This is to calculate the statistical parameters(mean, median, minimum, maximum and standard deviation)
 # The calculation is done for each of the numerical variables in the data set
 
-#this is to calculate the statistical parameters of the integer variable "population" from the data set
+#This is to calculate the statistical parameters of the integer variable "population" from the data set
 summary(covid_New$population)
+#This is to show the box plot with the outliers of the "population" column
 boxplot(covid_New$population)
+#This is to show the histogram of the "population" column
 hist(covid_New$population)
 
-#this is to calculate the standard deviation of the "population" integer variable from the data set
-calculate_standard_deviation <- sd(covid_New$population)
-print(calculate_standard_deviation)
+#This is to calculate the standard deviation of the "population" integer variable from the data set
+cal_sd <- sd(covid_New$population)
+print(cal_sd)
 
 #this is to calculate the statistical parameters of the integer variable "weekly_count" from the data set
 summary(covid_New$weekly_count)
-
-
+#This is to show the box plot with the outliers of the "weekly_count" column
+boxplot(covid_New$weekly_count)
+#This is to show the histogram of the "weekly_count" column
+hist(covid_New$weekly_count)
 #this is to calculate the standard deviation of the "weekly_count" integer variable from the data set
-calculate_standard_deviation <- sd(covid_New$weekly_count)
-print(calculate_standard_deviation)
+calc_sd <- sd(covid_New$weekly_count)
+print(calc_sd)
 
 #this is to calculate the statistical parameters of the numeric variable "rate_14_day" from the data set
 summary(covid_New$rate_14_day)
-
-
+#This is to show the box plot with the outliers of the "rate_14_day" column
+boxplot(covid_New$rate_14_day)
+#This is to show the histogram of the "rate_14_day" column
+hist(covid_New$rate_14_day)
 #this is to calculate the standard deviation of the "weekly_count" integer variable from the data set excluding missing values
-calculate_standard_deviation <- sd(covid_New$rate_14_day, na.rm = TRUE)
-print(calculate_standard_deviation)
+calc_sd <- sd(covid_New$rate_14_day)
+print(calc_sd)
 
 
-#this is to calculate the statistical parameters of the integer variable "cumulative_count" from the data set
+#This is to calculate the statistical parameters of the integer variable "cumulative_count" from the data set
 summary(covid_New$cumulative_count)
+#This is to show the box plot with the outliers of the "cumulative_count" column
+boxplot(covid_New$cumulative_count)
+#This is to show the histogram of the "cumulative_count" column
+hist(covid_New$cumulative_count)
+#This is to calculate the standard deviation of the "cumulative_count" integer variable from the data set excluding missing values
+calc_sd <- sd(covid_New$cumulative_count)
+print(cal_sd)
+
+# This is to import the library
+install.packages("caret")
+#This is to load the library
+library(caret)
+# This is to select the respectively columns of the data set
+num_cols <- c("population", "rate_14_day", "cumulative_count", "weekly_count")
+# This is to apply Min-Max Scaling
+covid_New[num_cols] <- sapply(covid_New[num_cols], function(x) ifelse((x), 0.5, (x - min(x)) / (max(x) - min(x))))
+# This is to view the head of the normalized data
+view(covid_New)
+
+# This is to select the respectively columns of the data set
+num_cols <- c("population", "rate_14_day", "cumulative_count", "weekly_count")
+# This is to find the Mean and Standard Deviation for each column
+mean_values <- colMeans(covid_New[num_cols])
+sd_values <- apply(covid_New[num_cols], 2, sd)
+# This is to apply z-score Standardization
+covid_New[num_cols] <- scale(covid_New[num_cols], center = mean_values, scale = sd_values)
+# This is to view the standardized data
+view(covid_New)
+
+# This is the Robust Scalar Calculation
+# This is extracting numerical columns
+num_cols <- c("population", "rate_14_day", "cumulative_count", "weekly_count")
+# This is to apply the method Robust Scaling
+pp_params <- preProcess(covid_New[num_cols], method = c("center", "scale"))
+# This is to apply the learned parameters to the original data
+covid_New_robust <- predict(pp_params, newdata = covid_New[num_cols])
+# This is to view the head of the Robust Scaled data
+view(covid_New_robust)
+
+#This is a scatter Plot for the correlation of Weekly Count Vs Rate 14 Day in Ireland
+# This is to filter data for Ireland
+eir_info <- covid_New[covid_New$country == "Ireland", ]
+# This is to load the library
+library(ggplot2)
+#This is to show off the plot
+ggplot(eir_info, aes(x = weekly_count, y = rate_14_day)) +
+  geom_point() +
+  labs(title = "Scatter Plot: Weekly Count vs Rate 14 Day (Ireland)",
+       x = "weekly_count",
+       y = "rate_14_day")
+
+# This is to show the correlation for All Countries in the EU Zone to show off Rate 14 Day Over Time
+library(ggplot2)
+ggplot(covid_New, aes(x = year_week, y = rate_14_day, group = 1)) +
+  geom_line() +
+  labs(title = "Line Plot: Rate 14 Day Over Time For All Countries in Europe",
+       x = "Year-Week",
+       y = "Rate 14 Day")
 
 
-#this is to calculate the standard deviation of the "cumulative_count" integer variable from the data set excluding missing values
-calculate_standard_deviation <- sd(covid_New$cumulative_count, na.rm = TRUE)
-print(calculate_standard_deviation)
+#This is to show the correlation in Ireland using Line Plot to show-off Rate 14 Day Over Time
+# This is to filtering data in Ireland
+ireland <- covid_New[covid_New$country == "Ireland", ]
+# This is the line plot for Ireland
+library(ggplot2)
+ggplot(ireland, aes(x = year_week, y = rate_14_day, group = 1)) +
+  geom_line() +
+  labs(title = "Line Plot: Rate 14 Day Over Time in Ireland",
+       x = "Year-Week",
+       y = "Rate 14 Day")
 
 
+# This is to calculate the correlation matrix between the features of the data set
+corr_mat <- cor(covid_New[, c('population', 'weekly_count', 'rate_14_day', 'cumulative_count')], use = "complete.obs")
+
+# This is to visualize the correlation matrix with a heat map
+library(corrplot)
+corrplot(corr_mat, method = "color", type = "upper", tl.col = "black", tl.srt = 45)
